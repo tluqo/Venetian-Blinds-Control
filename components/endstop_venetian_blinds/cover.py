7a8b4dfc17b50ed27edb0654d26dc1a16265d6a9
tluqo <tluqoo@zoho.com>
2023-07-24 23:31:40 +0200

    Update cover.py

--- components/endstop_venetian_blinds/cover.py
+++ components/endstop_venetian_blinds/cover.py
@@ -18,11 +18,11 @@ CONF_TILT_DURATION = "tilt_duration"
 CONF_ACTUATOR_ACTIVATION_DURATION = "actuator_activation_duration"
 
 endstop_venetian_blinds_ns = cg.esphome_ns.namespace('endstop_venetian_blinds')
-EndstopVenetianBlinds = venetian_blinds_ns.class_('EndstopVenetianBlinds', cover.Cover, cg.Component)
+EndstopVenetianBlinds = endstop_venetian_blinds_ns.class_('EndstopVenetianBlinds', cover.Cover, cg.Component)
 
 CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(EndstopVenetianBlinds),
-#    cv.Required(CONF_OPEN_ENDSTOP): cv.use_id(binary_sensor.BinarySensor),
+    cv.Required(CONF_OPEN_ENDSTOP): cv.use_id(binary_sensor.BinarySensor),
     cv.Required(CONF_OPEN_ACTION): automation.validate_automation(single=True),
     cv.Required(CONF_OPEN_DURATION): cv.positive_time_period_milliseconds,
     cv.Required(CONF_CLOSE_ENDSTOP): cv.use_id(binary_sensor.BinarySensor),
@@ -36,30 +36,21 @@ CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await cover.register_cover(var, config)
 
-    yield cg.register_component(var, config)
-    yield cover.register_cover(var, config)
-    yield automation.build_automation(
-        var.get_stop_trigger(), [], config[CONF_STOP_ACTION]
-    )
-    cg.add(var.set_open_duration(config[CONF_OPEN_DURATION]))
+    await automation.build_automation(var.get_stop_trigger(), [], config[CONF_STOP_ACTION])
 
-    bin = yield cg.get_variable(config[CONF_OPEN_ENDSTOP])
+    bin = await cg.get_variable(config[CONF_OPEN_ENDSTOP])
     cg.add(var.set_open_endstop(bin))
     cg.add(var.set_open_duration(config[CONF_OPEN_DURATION]))
+    await automation.build_automation(var.get_open_trigger(), [], config[CONF_OPEN_ACTION])
 
-    yield automation.build_automation(
-        var.get_open_trigger(), [], config[CONF_OPEN_ACTION]
-    )
-    cg.add(var.set_close_duration(config[CONF_CLOSE_DURATION]))
-
-    bin = yield cg.get_variable(config[CONF_CLOSE_ENDSTOP])
+    bin = await cg.get_variable(config[CONF_CLOSE_ENDSTOP])
     cg.add(var.set_close_endstop(bin))
     cg.add(var.set_close_duration(config[CONF_CLOSE_DURATION]))
+    await automation.build_automation(var.get_close_trigger(), [], config[CONF_CLOSE_ACTION])
 
-    yield automation.build_automation(
-        var.get_close_trigger(), [], config[CONF_CLOSE_ACTION]
-    )
     cg.add(var.set_tilt_duration(config[CONF_TILT_DURATION]))
     cg.add(var.set_actuator_activation_duration(config[CONF_ACTUATOR_ACTIVATION_DURATION]))
     cg.add(var.set_assumed_state(config[CONF_ASSUMED_STATE]))
